5511c0e0929ca6b39fb1b610a7c04dca2e409287
FedorovVladimir <vladimir.fodorow@gmail.com>
2021-09-07 19:24:25 +0700

    новая система ссылок, опход в ширину для решения задачи

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 //Cube кубик
 type Cube struct {
 	State int // 0 - точка, 1, 2, 3 - пустые грани
-	links []*Cube
+	links []int
 }
 
 // поворот кубика
@@ -17,27 +20,74 @@ func (c *Cube) rotate() {
 }
 
 //RotateDeep поворот кубика и связаных кубиков
-func (c *Cube) RotateDeep() {
+func (c *Cube) RotateDeep(cubes []Cube) {
 	c.rotate()
 	for _, link := range c.links {
-		link.rotate()
+		cubes[link].rotate()
 	}
 }
 
 //Add добавление связи
-func (c *Cube) Add(other *Cube) {
+func (c *Cube) Add(other int) {
 	c.links = append(c.links, other)
+}
+
+type Step struct {
+	cubes          []Cube
+	nextCubeNumber int
+	path           []int
+}
+
+var steps []Step
+
+func display(prefix string, cubes []Cube) {
+	fmt.Print(prefix)
+	for _, cube := range cubes {
+		fmt.Printf("%v ", cube.State)
+	}
+	fmt.Println()
+}
+
+func isWin(cubes []Cube) bool {
+	win := true
+	for i := 0; i < len(cubes)-1; i++ {
+		if cubes[i].State != cubes[i+1].State {
+			win = false
+		}
+	}
+	return win
+}
+
+func findPath() {
+	for len(steps) > 0 {
+		step := steps[0]
+		steps = steps[1:]
+		step.cubes[step.nextCubeNumber].RotateDeep(step.cubes)
+		display("("+strconv.Itoa(step.nextCubeNumber)+") -> ", step.cubes)
+		if isWin(step.cubes) {
+			display("end ", step.cubes)
+			fmt.Println("path:", step.path)
+			return
+		}
+		for i := 0; i < len(step.cubes); i++ {
+			tmp := make([]Cube, len(step.cubes))
+			copy(tmp, step.cubes)
+			steps = append(steps, Step{cubes: tmp, nextCubeNumber: i, path: append(step.path, i)})
+		}
+	}
 }
 
 func main() {
 	n := 5
 	cubes := make([]Cube, n)
-	cubes[1].RotateDeep()
-	cubes[0].Add(&cubes[1])
-	cubes[0].Add(&cubes[2])
-	cubes[0].RotateDeep()
+	cubes[1].RotateDeep(cubes)
+	cubes[1].Add(2)
 
-	for _, cube := range cubes {
-		fmt.Println(cube)
+	for i := 0; i < n; i++ {
+		tmp := make([]Cube, len(cubes))
+		copy(tmp, cubes)
+		steps = append(steps, Step{cubes: tmp, nextCubeNumber: i, path: []int{i}})
 	}
+	display("start ", cubes)
+	findPath()
 }
